2230192dfe16278bfbcf4345a6c08403c9a4ed80
Yixiao <yixiaozhouchen@hotmail.com>
2018-07-31 23:40:10 -0400

    Initial commit

---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.app')
+
+@section('content')
+        <h1>{{$title}}</h1>
+        @if(count($services)>0)
+                <ul>
+                @foreach($services as $services)
+                        <li>{{$services}}</li>
+
+                @endforeach
+                </ul>
+
+        
+        @endif
+        @endsection
